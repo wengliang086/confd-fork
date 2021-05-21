e172bccb5cbbdf8e9cde9025eba42131b01a0a8c
yongxiang.li <wengliang_1986@126.com>
2021-05-21 16:40:59 +0800

    更新redis

--- backends/redis/client.go
+++ backends/redis/client.go
@@ -21,6 +21,8 @@ type Client struct {
 	machines  []string
 	password  string
 	separator string
+	psc       redis.PubSubConn
+	pscChan   chan watchResponse
 }
 
 // Iterate through `machines`, trying to connect to each in turn.
@@ -86,6 +88,7 @@ func NewRedisClient(machines []string, password string, separator string) (*Clie
 	var err error
 	clientWrapper := &Client{}
 	clientWrapper.client, _, err = tryConnect(machines, password, true)
+	clientWrapper.machines = machines
 	return clientWrapper, err
 }
 
@@ -105,10 +108,182 @@ func (c *Client) clean(key string) string {
 	return strings.Replace(k, c.separator, "/", -1)
 }
 
+func (c *Client) connectedClient() (redis.Conn, error) {
+	if c.client != nil {
+		log.Debug("Testing existing redis connection.")
+
+		resp, err := c.client.Do("PING")
+		if (err != nil && err == redis.ErrNil) || resp != "PONG" {
+			log.Error(fmt.Sprintf("Existing redis connection no longer usable. "+
+				"Will try to re-establish. Error: %s", err.Error()))
+			c.client = nil
+		}
+	}
+
+	// Existing client could have been deleted by previous block
+	if c.client == nil {
+		var err error
+		c.client, _, err = tryConnect(c.machines, c.password, true)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return c.client, nil
+}
+
 func (c Client) GetValues(keys []string) (map[string]string, error) {
-	panic("implement me")
+	// Ensure we have a connected redis client
+	rClient, err := c.connectedClient()
+	if err != nil && err != redis.ErrNil {
+		return nil, err
+	}
+
+	vars := make(map[string]string)
+	for _, key := range keys {
+		key = strings.Replace(key, "/*", "", -1)
+
+		k := c.transform(key)
+		t, err := redis.String(rClient.Do("TYPE", k))
+
+		if err == nil && err != redis.ErrNil {
+
+			if t == "string" {
+				value, err := redis.String(rClient.Do("GET", k))
+				if err == nil {
+					vars[key] = value
+					continue
+				}
+				if err != redis.ErrNil {
+					return vars, err
+				}
+			} else if t == "hash" {
+				idx := 0
+				for {
+					values, err := redis.Values(rClient.Do("HSCAN", k, idx, "MATCH", "*", "COUNT", "1000"))
+					if err != nil && err != redis.ErrNil {
+						return vars, err
+					}
+					idx, _ = redis.Int(values[0], nil)
+					items, _ := redis.Strings(values[1], nil)
+					for i := 0; i < len(items); i += 2 {
+						var newKey, value string
+						if newKey, err = redis.String(items[i], nil); err != nil {
+							return vars, err
+						}
+						if value, err = redis.String(items[i+1], nil); err != nil {
+							return vars, err
+						}
+						vars[c.clean(k+"/"+newKey)] = value
+					}
+					if idx == 0 {
+						break
+					}
+				}
+			} else {
+				if key == "/" {
+					k = "*"
+				} else {
+					k = fmt.Sprintf(c.transform("%s/*"), k)
+				}
+
+				idx := 0
+				for {
+					values, err := redis.Values(rClient.Do("SCAN", idx, "MATCH", k, "COUNT", "1000"))
+					if err != nil && err != redis.ErrNil {
+						return vars, err
+					}
+					idx, _ = redis.Int(values[0], nil)
+					items, _ := redis.Strings(values[1], nil)
+					for _, item := range items {
+						var newKey string
+						if newKey, err = redis.String(item, nil); err != nil {
+							return vars, err
+						}
+						if value, err := redis.String(rClient.Do("GET", newKey)); err == nil {
+							vars[c.clean(newKey)] = value
+						}
+					}
+					if idx == 0 {
+						break
+					}
+				}
+			}
+		} else {
+			return vars, err
+		}
+	}
+
+	log.Debug(fmt.Sprintf("Key Map: %#v", vars))
+
+	return vars, nil
 }
 
 func (c Client) WatchPrefix(prefix string, keys []string, waitIndex uint64, stopChan chan bool) (uint64, error) {
-	panic("implement me")
+	if waitIndex == 0 {
+		return 1, nil
+	}
+
+	if len(c.pscChan) > 0 {
+		var respChan watchResponse
+		for len(c.pscChan) > 0 {
+			respChan = <-c.pscChan
+		}
+		return respChan.waitIndex, respChan.err
+	}
+
+	go func() {
+		if c.psc.Conn == nil {
+			rClient, db, err := tryConnect(c.machines, c.password, false)
+
+			if err != nil {
+				c.psc = redis.PubSubConn{Conn: nil}
+				c.pscChan <- watchResponse{0, err}
+				return
+			}
+
+			c.psc = redis.PubSubConn{Conn: rClient}
+
+			go func() {
+				defer func() {
+					c.psc.Close()
+					c.psc = redis.PubSubConn{Conn: nil}
+				}()
+				for {
+					switch n := c.psc.Receive().(type) {
+					case redis.PMessage:
+						log.Debug(fmt.Sprintf("Redis Message: %s %s\n", n.Channel, n.Data))
+						data := string(n.Data)
+						commands := [12]string{"del", "append", "rename_from", "rename_to", "expire", "set", "incrby", "incrbyfloat", "hset", "hincrby", "hincrbyfloat", "hdel"}
+						for _, command := range commands {
+							if command == data {
+								c.pscChan <- watchResponse{1, nil}
+								break
+							}
+						}
+					case redis.Subscription:
+						log.Debug(fmt.Sprintf("Redis Subscription: %s %s %d\n", n.Kind, n.Channel, n.Count))
+						if n.Count == 0 {
+							c.pscChan <- watchResponse{0, nil}
+							return
+						}
+					case error:
+						log.Debug(fmt.Sprintf("Redis error: %v\n", n))
+						c.pscChan <- watchResponse{0, n}
+						return
+					}
+				}
+			}()
+
+			c.psc.PSubscribe("__keyspace@" + strconv.Itoa(db) + "__:" + c.transform(prefix) + "*")
+		}
+	}()
+
+	select {
+	case <-stopChan:
+		c.psc.PUnsubscribe()
+		return waitIndex, nil
+	case r := <-c.pscChan:
+		return r.waitIndex, r.err
+	}
 }
